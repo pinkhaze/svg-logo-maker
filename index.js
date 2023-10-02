550db6373462a9493140dc66bda739d9a3b40f21
Monika <raciam@oregonstate.edu>
2023-10-02 18:58:02 -0500

    Add switch statement and logic for generating SVG string

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-// const questions = require('./lib/questions.js')
-
-// function writeToFile(fileName, data) {
-//     fs.writeFile(fileName, generateMarkdown(data), (err) => 
-//     err ? console.log(err) : console.log('Success! Your README file is located in the dist folder.')
-//     );
-// }
-
-// // Function to initialize app
-// function init() {
-//     inquirer
-//     .prompt(questions)
-//     .then((answers) => {
-//         writeToFile('./dist/test.', answers);   
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     })
-// };
-
-// // Function call to initialize app
-
-const { questions } = require('./lib/questions');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => 
-    err ? console.log(err) : console.log('Success! Your SVG logo file is located in the dist folder.')
-    );
-}
+const { questions } = require('./lib/questions');
+const { Circle, Square, Triangle } = require('./lib/shapes.js');
 
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => 
+  err ? console.log(err) : console.log('Success! Your SVG logo file is located in the dist folder.')
+  );
+}
 
 function init() {
     inquirer
     .prompt(questions)
   .then(answers => {
- 
-    writeToFile('./dist/logo.svg', answers);
+
+    let userShape;
+
+    switch (answers.logoShape) {
+      case 'Triangle':
+        userShape = new Triangle();
+        break;
+      case 'Circle':
+        userShape = new Circle();
+        break;
+      case 'Square':
+        userShape = new Square();
+        break;
+    }
+
+    const { shapeColor, logoText, textColor } = answers;
+
+    userShape.setColor(shapeColor);
+
+    const svgEl = `
+      <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${userShape.render()}
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">
+          ${logoText}
+        </text>
+      </svg>
+    `;
+
+    writeToFile(`./dist/logo.svg`, svgEl);
   })
 }
 
 init();
+
